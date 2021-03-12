7de728339db9eba3a11e0b49cb0419daf127c50f
kadet <techkadet@gmail.com>
2021-03-12 17:58:29 +0100

    removed onload timeout

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,17 +5,16 @@ const preloadContainer = document.querySelector("#preloader");
 const products = document.querySelectorAll(".product-grid__card");
 
 window.onload = () => {
-  setTimeout(() => {
 
-    // add animated classes to each product card
-    products.forEach((product, index) => {
-      product.classList.add(`slide-up-${index + 1}`)
-    })
+  // add animated classes to each product card
+  products.forEach((product, index) => {
+    product.classList.add(`slide-up-${index + 1}`);
+  });
+
+  // fade out preloader on page load
+  preloadContainer.classList.add("fade-out");
+
+  // enable page scrolling
+  body.classList.add("overflow");
   
-    // fade out preloader on page load
-    preloadContainer.classList.add("fade-out");
-    
-    // enable page scrolling
-    body.classList.add("overflow")
-  }, 1000);
-}
+};
